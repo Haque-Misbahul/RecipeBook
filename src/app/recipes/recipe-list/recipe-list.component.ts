828d5fe4c1f3d1c87021bc3cb330172d037433cb
Haque Misbahul (DC/SIC) <ham1ul@bosch.com>
2024-03-13 11:19:14 +0100

    adding dropdown feature

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,17 +10,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit{
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe [] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://preview.redd.it/3splx63nx8c01.png?width=1080&crop=smart&auto=webp&s=bbb5413d0fefc1697b1cd1e142301911dca24262'),
-  
-    new Recipe('Another Test Recipe', 'This is delicious', 'https://preview.redd.it/3splx63nx8c01.png?width=1080&crop=smart&auto=webp&s=bbb5413d0fefc1697b1cd1e142301911dca24262')
-  ];
+  recipes: Recipe [];
 
-  constructor(){
-
+  constructor(private recipeService: RecipeService){
   }
+
   ngOnInit() {
-    
+    this.recipes = this.recipeService.getRecipe();
   }
   onRecipeSelected(recipe: Recipe){
     this.recipeWasSelected.emit(recipe);
